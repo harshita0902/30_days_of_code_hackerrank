5df67ca7043987e3ed1cbbcf5087e850ce1e2892
harshita0902 <hpandey.652@gmail.com>
2020-04-15 13:35:37 +0530

    added datatype file

---
+++ datatype.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+a=int(input())
+b=float(input())
+c=input() #Declare second integer, double, and String variables.
+print(a+i)
+print(b+d)
+print(s+c)
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
